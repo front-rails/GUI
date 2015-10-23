264ce7bdb8444abb86aac2ead62eadddc9fd35a0
Lacey R <laceyrice@me.com>
2015-10-23 13:22:51 -0400

    working baby steps

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,24 @@
 /* PSEUDO CODE FOR LOGIN MENU
 
 1. COLLECT DATA ENTERED INTO THE <INPUT>
-2. WHEN .LOGIN-SUBMIT IS 'CLICKED' SEND INFO TO DATABASE TO BE VERIFIED?
+2. WHEN .LOGIN-SUBMIT IS 'CLICKED'  `GET/users/id` TO DATABASE TO BE VERIFIED?
+*/
+
+
+;(function(){
+  angular.module('Front-Rails', [ ])
+
+.run(function($http, $rootScope) {
+  $http.get('https://stackundertow.herokuapp.com/questions')
+    .then(function(response){
+      // $rootScope.query = "hello";
+
+       $rootScope.query = response.data[0].query;
+
+      // $rootScope.questions = response.data;
+    })
+
+})
+
+
+})();
