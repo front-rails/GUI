fa240760327c4a9a0917687dd490e192e853c06f
Shay <hallshay@gmail.com>
2015-11-30 16:07:52 -0500

    Questions in question list redirect to thier own page.

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@
       templateUrl: 'partials/top-questions.html',  // controller: 'questionList'
     })
 
-    .when('/question', {
+    .when('/question/:id', {
       templateUrl: 'partials/question.html',  // controller: 'questionPage'
     })
   }) //END $routeProvider
@@ -39,9 +39,12 @@
 
   .controller("questionAsker", function($scope, $http){ //CONTROLLER FOR submitting answer
     $scope.question = {
-      description: ''
+      query: '',
+      description: '',
+      user_id: 'user_id'
     }
-    $scope.submitQuestion = function() {
+    $scope.submitQuestion = function(id) {
+      user_id = $;
       $http.post('https://stackundertow.herokuapp.com/questions', $scope.question)
         .then(function(){
           $scope.question = {
@@ -51,7 +54,19 @@
     };
   })//CONTROLLER FOR submitting question
 
-  .controller("OfferAnswer", function($scope, $http){ //CONTROLLER FOR submitting answer
+  .controller("OfferAnswer", function($scope, $http, $routeParams, $rootScope){ //CONTROLLER FOR submitting answer
+      console.log($routeParams);
+      var id = $routeParams.id;
+      var username = $routeParams.username;
+      $scope.username = $routeParams.username;
+      $scope.id = $routeParams.id;
+      $http.get('https://stackundertow.herokuapp.com/questions/' + id + "/")
+        .then(function(response){
+          $rootScope.question = response.data;
+          $rootScope.query = response.data.query;
+          $rootScope.description = response.data.description;
+      })
+
     $scope.answers =     {user_id: 6, question_id: 1, description: "This is my answer from user 6 to question 1, right?"
     }
     $scope.submit = function() {
@@ -59,19 +74,23 @@
     };
   })//CONTROLLER FOR submitting answer
 
-  .controller('loginCtrl', function($scope, $http){//CONTROLLER FOR LOGIN
+  .controller('loginCtrl', function($scope, $http, $window){//CONTROLLER FOR LOGIN
     $scope.user = {
       email: '',
       password: ''
     }
     $scope.submit= function(){
       $http.post('https://stackundertow.herokuapp.com/sessions', $scope.user)
-      .then (function() {
-        $scope.user = {
-          email: '',
-          password: ''
-        }
-      })
+        .success(function (data, status, headers, config) {
+          $window.sessionStorage.auth_token = data.auth_token;
+          $scope.user = {
+            email: '',
+            password: ''
+          }
+        })
+      .error(function (data, status, headers, config) {
+        alert("Error: Invalid user or password");
+      });
     }
   })//CONTROLLER FOR LOGIN
 
