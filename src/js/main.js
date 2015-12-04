86adf99090f1352f82d1fdc73ece05340014a216
Shay <hallshay@gmail.com>
2015-12-04 09:35:10 -0500

    Combine questionList and questionAsker controllers. 2 Way bind answer votes

--- src/js/main.js
+++ src/js/main.js
@@ -15,75 +15,6 @@
     })
   }) //END $routeProvider
 
-  .controller('questionList', function($scope, $routeParams, $http){  //CONTROLLER FOR POPULATING TOP QUESTIONS
-    $http.get('https://stackundertow.herokuapp.com/questions')
-      .then(function(response){
-        $scope.questions = response.data;
-      })
-  })//CONTROLLER FOR POPULATING TOP QUESTIONS
-
-  .controller("SignupController", function($scope, $http){ //CONTROLLER FOR SIGNUP
-    $scope.user = {
-      name: '',
-      email: '',
-      password: ''
-    }
-    $scope.submit = function() {
-      $http.post('https://stackundertow.herokuapp.com/users', $scope.user);
-    };
-    $scope.user = {
-      name: '',
-      email: '',
-      password: ''
-    };
-  })//CONTROLLER FOR SIGNUP
-
-  .controller("questionAsker", function($scope, $http, $rootScope){ //CONTROLLER FOR submitting answer
-
-    $rootScope.submitQuestion = function(puzzle, detail) {
-      $rootScope.question = {
-        query: puzzle,
-        description: detail,
-        user_id: $rootScope.id
-      }
-      $http.post('https://stackundertow.herokuapp.com/questions', $rootScope.question)
-        .then(function(){
-          $scope.question = {
-            query: '',
-            description: ''
-          }
-        })
-    };
-  })//CONTROLLER FOR submitting question
-
-  .controller("OfferAnswer", function($scope, $http, $routeParams, $rootScope){ //CONTROLLER FOR submitting answer
-      var id = parseInt($routeParams.id, 10);
-      var username = $routeParams.username;
-      $scope.username = $routeParams.username;
-      $scope.id = $routeParams.id;
-      $http.get('https://stackundertow.herokuapp.com/questions/' + id + "/")
-        .then(function(response){
-          $rootScope.question = response.data;
-          $rootScope.query = response.data.query;
-          $rootScope.description = response.data.description;
-      })
-
-
-    $rootScope.submitAnswer = function(deets) {
-      $rootScope.answers =  {
-        user_id: $rootScope.id,
-        question_id: id,
-        description: deets
-      }
-      $http.post('https://stackundertow.herokuapp.com/answers', $rootScope.answers)
-      .then(function(){
-        $scope.answers = {
-          description: ''
-        }
-      })
-    };
-  })//CONTROLLER FOR submitting answer
-
   .controller('loginCtrl', function($scope, $http, $window, $rootScope){//CONTROLLER FOR LOGIN
     $scope.user = {
       email: '',
@@ -107,16 +38,84 @@
     }
   })//CONTROLLER FOR LOGIN
 
+  .controller("SignupController", function($scope, $http){ //CONTROLLER FOR SIGNUP
+    $scope.user = {
+      name: '',
+      email: '',
+      password: ''
+    }
+    $scope.submit = function() {
+      $http.post('https://stackundertow.herokuapp.com/users', $scope.user);
+    };
+    $scope.user = {
+      name: '',
+      email: '',
+      password: ''
+    };
+  })//CONTROLLER FOR SIGNUP
+
+  .controller('questionList', function($scope, $routeParams, $http){  //CONTROLLER FOR POPULATING TOP QUESTIONS
+    $http.get('https://stackundertow.herokuapp.com/questions')
+      .then(function(response){
+        $scope.questions = response.data;
+      })
+
+    $scope.submitQuestion = function(puzzle, detail) {
+      $scope.question = {
+        query: puzzle,
+        description: detail,
+        user_id: $scope.id
+      }
+      $http.post('https://stackundertow.herokuapp.com/questions', $scope.question)
+        .then(function(){
+          $scope.question = {
+            query: '',
+            description: ''
+          }
+        })
+    };
+  })//CONTROLLER FOR POPULATING TOP QUESTIONS
+
+  .controller("OfferAnswer", function($scope, $http, $routeParams, $rootScope){ //CONTROLLER FOR submitting answer
+      var id = parseInt($routeParams.id, 10);
+      var username = $routeParams.username;
+      $scope.username = $routeParams.username;
+      $scope.id = $routeParams.id;
+      $http.get('https://stackundertow.herokuapp.com/questions/' + id + "/")
+        .then(function(response){
+          $rootScope.question = response.data;
+          $rootScope.query = response.data.query;
+          $rootScope.description = response.data.description;
+      })
+
+    $rootScope.submitAnswer = function(deets) {
+      $scope.answers =  {
+        user_id: $scope.id,
+        question_id: id,
+        description: deets
+      }
+      $http.post('https://stackundertow.herokuapp.com/answers', $scope.answers)
+      .then(function(){
+        $scope.answers = {
+          description: ''
+        }
+      })
+    };
+  })//CONTROLLER FOR submitting answer
+
   .controller('vote', function($scope, $http) {
 
     $scope.upVote = function() {
     var id = $scope.answer.id;
       $http.patch('https://stackundertow.herokuapp.com/answers/'+ id +'/upvote')
+        $scope.answer.votes_quality +=1;
     };
 
     $scope.downVote = function() {
     var id = $scope.answer.id;
       $http.patch('https://stackundertow.herokuapp.com/answers/'+ id +'/downvote')
+        $scope.answer.votes_quality -=1;
+
     };
 
   })//CONTROLLER FOR VOTING
